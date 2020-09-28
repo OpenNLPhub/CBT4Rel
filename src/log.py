b2edd0e299ec57caf8f381de5c2488e4738be73e
Zreal <zenglz_pro@163.com>
2020-09-28 17:08:03 +0800

    finish log.py

---
+++ src/log.py
@@ -0,0 +1,31 @@
+'''
+ * @author Waldinsamkeit
+ * @email Zenglz_pro@163.com
+ * @create date 2020-09-28 15:24:23
+ * @desc 
+'''
+
+import logging
+from datetime import datetime
+import os
+
+
+logger =  logging.getLogger('WenQA_DGCNN')
+logger.setLevel(logging.INFO)
+
+fmt = '[%(asctime)s]-[%(levelname)s]-%(message)s'
+formatter=logging.Formatter(fmt)
+
+handle_stream =  logging.StreamHandler()
+handle_stream.setFormatter(formatter)
+handle_stream.setLevel(logging.INFO)
+
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+cwd = os.getcwd()
+log_path = os.path.join(cwd,'log',now+'.log')
+handle_file = logging.FileHandler(filename = log_path,encoding = 'utf-8',mode = 'w')
+handle_file.setFormatter(formatter)
+handle_file.setLevel(logging.INFO)
+
+logger.addHandler(handle_file)
+logger.addHandler(handle_stream)
